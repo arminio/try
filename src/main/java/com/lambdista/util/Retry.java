677c976011c7482ccc358f177bb3689c91a36a25
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2015-08-06 23:59:45 +1000

    adding valid exceptions that will cause a retry

--- src/main/java/com/lambdista/util/Retry.java
+++ src/main/java/com/lambdista/util/Retry.java
@@ -1,15 +1,19 @@
 package com.lambdista.util;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 /**
  * Created by armin on 06/08/15.
  */
-public class Retry {
+public class Retry<T, R extends Exception> {
 
     static int someCounter = 5;
+
     public static void main(String[] args) {
         String x = "trying...";
+
+
         FailableSupplier<Integer> integerFailableSupplier = () -> {
             System.out.println(x + someCounter--);
             if (someCounter > 3) {
@@ -17,16 +21,31 @@ public class Retry {
             }
             return someCounter;
         };
-        System.out.println(retry(5, 0, integerFailableSupplier));
+        System.out.println(new Retry().retry(5, 0, integerFailableSupplier));
     }
 
-    private static <T> T retry(int maxRetries, int retryCount, FailableSupplier<T> supplier) {
+    public T retry(int maxRetries, int retryCount, FailableSupplier<T> supplier, R... onlyForTheseExceptions) {
 
         Try<T> aTry = Try.apply(supplier);
-        if (aTry.isFailure() && maxRetries > retryCount) {
+        if (isValidException(aTry, onlyForTheseExceptions) && maxRetries > retryCount) {
             return retry(maxRetries, retryCount++, supplier);
         } else {
             return aTry.get();
+        }
+    }
+
+    private boolean isValidException(Try<T> aTry, R... onlyForTheseExceptions) {
+        boolean isFailure = aTry.isFailure();
+        if (isFailure) {
+            if (onlyForTheseExceptions.length > 0) {
+                // only return true if the caught exception is in the list of the ones we care about
+                // TODO: should we check for subclasse?
+                return Arrays.asList(onlyForTheseExceptions).contains(aTry.failed().get().getClass());
+            } else {
+                return true;
+            }
+        } else {
+            return false;
         }
     }
 
